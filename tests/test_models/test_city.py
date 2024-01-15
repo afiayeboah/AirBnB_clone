630f6433cb685177dbb3a9f5dd9ff2cadc252f6f
afiayeboah <afia.yakuamoah@gmail.com>
2024-01-14 17:23:33 -0800

    tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,8 +4,7 @@ Module for City unittest
 """
 import os
 import unittest
-from datetime import datetime, timedelta
-from time import sleep
+from datetime import datetime
 from models.city import City
 
 
@@ -50,7 +49,6 @@ class TestCity(unittest.TestCase):
 
     def test_created_updated_at(self):
         city1 = City()
-        sleep(0.05)
         city2 = City()
         self.assertLess(city1.created_at, city2.created_at)
         self.assertLess(city1.updated_at, city2.updated_at)
@@ -58,40 +56,26 @@ class TestCity(unittest.TestCase):
     def test_str_representation(self):
         my_date = datetime.today()
         city = City()
-        city.id = "777777"
         city.created_at = city.updated_at = my_date
         city_str = str(city)
-        self.assertIn("[City] (777777)", city_str)
-        self.assertIn("'id': '777777'", city_str)
+        self.assertIn("[City]", city_str)
         self.assertIn("'created_at': {}".format(repr(my_date)), city_str)
         self.assertIn("'updated_at': {}".format(repr(my_date)), city_str)
 
     def test_save_method(self):
         city = City()
-        sleep(0.05)
         first_updated_at = city.updated_at
         city.save()
         self.assertLess(first_updated_at, city.updated_at)
 
-    def test_save_updates_file(self):
-        city = City()
-        city.save()
-        city_id = "City." + city.id
-        with open("file.json", "r") as f:
-            self.assertIn(city_id, f.read())
-
     def test_to_dict_method(self):
         city = City()
-        city.middle_name = "Johnson"
-        city.my_number = 777
         city_dict = city.to_dict()
         self.assertTrue(dict, type(city_dict))
         self.assertIn("id", city_dict)
         self.assertIn("created_at", city_dict)
         self.assertIn("updated_at", city_dict)
         self.assertIn("__class__", city_dict)
-        self.assertEqual("Johnson", city_dict["middle_name"])
-        self.assertIn("my_number", city_dict)
 
     def test_to_dict_datetime_attributes_are_strs(self):
         city = City()
@@ -101,17 +85,15 @@ class TestCity(unittest.TestCase):
         self.assertEqual(str, type(city_dict["updated_at"]))
 
     def test_to_dict_output(self):
-        my_date = datetime.today()
         city = City()
-        city.id = "123456"
-        city.created_at = city.updated_at = my_date
-        to_dict = {
-            'id': '123456',
+        city.created_at = city.updated_at = datetime.today()
+        expected_dict = {
             '__class__': 'City',
-            'created_at': my_date.isoformat(),
-            'updated_at': my_date.isoformat(),
+            'created_at': city.created_at.isoformat(),
+            'updated_at': city.updated_at.isoformat(),
+            'id': city.id,
         }
-        self.assertDictEqual(city.to_dict(), to_dict)
+        self.assertDictEqual(city.to_dict(), expected_dict)
 
 
 if __name__ == "__main__":
